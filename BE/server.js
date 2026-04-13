6c2dcab9a2661cd4f913819d1bd64daa67204b73
Kartik Sharma <poojan4604@gmail.com>
2026-04-13 12:01:38 +0530

    Update server.js

--- BE/server.js
+++ BE/server.js
@@ -1,9 +1,22 @@
 const express=require('express');
 const app=express();
 const port=3009;
+const router=express.Router();
+app.use(express.json());
+
 app.get('/',(req,res)=>{
     res.send("hello");
 });
+
+app.post('/link',(req,res)=>{
+   const {message}=req.body;
+   console.log(message);
+   res.status(201).json({
+    message:'Hey',
+    data:message
+   })
+
+})
 app.listen(port,()=>{
     console.log('Express app');
 });
