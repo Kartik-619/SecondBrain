3025b06dec315c73b64850e3338750402aa548e1
Kartik Sharma <poojan4604@gmail.com>
2026-04-14 15:20:07 +0530

    updateURL

--- BE/routes/URL.js
+++ BE/routes/URL.js
@@ -6,5 +6,6 @@ const readURL=require('../controller/URL/fetchURL.js');
 router.post('/writeURL',getURL);
 router.get('/readURL',readURL);
 router.delete('/deleteURL',require('../controller/URL/deleteURL.js'));
+router.patch('/updateURL',require('../controller/URL/updateURL.js'));
 
 module.exports=router;
