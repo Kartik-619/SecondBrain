6bdbe1ef67f05474bbe31f38398b72a9dffe3af6
Kartik Sharma <poojan4604@gmail.com>
2026-04-18 13:04:37 +0530

    Update POST.js

--- BE/routes/POST.js
+++ BE/routes/POST.js
@@ -3,4 +3,5 @@ const router=express.Router();
 
 router.post('/writePost',require('../controller/POSTS/savePost'));
 router.patch('/updatePost',require('../controller/POSTS/updatePost'));
+
 module.exports=router;
