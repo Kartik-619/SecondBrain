0d63efa9291783136397a107fa7a90a59bda7433
Kartik Sharma <poojan4604@gmail.com>
2026-04-20 11:36:24 +0530

    documented writePost.js

--- BE/routes/POST.js
+++ BE/routes/POST.js
@@ -1,9 +1,53 @@
-const express=require('express');
-const router=express.Router();
+const express = require('express');
+const router = express.Router();
 
-router.post('/writePost',require('../controller/POSTS/savePost'));
-router.patch('/updatePost',require('../controller/POSTS/updatePost'));
-router.get('/getPost',require('../controller/POSTS/fetchPost'));
-router.delete('/deletePost',require('../controller/POSTS/deletePost'));
+/**
+ * @swagger
+ * /writePost:
+ *   post:
+ *     summary: Create a new post
+ *     description: Add a new post to the database with title, message and linked to an author via authorId
+ *     tags:
+ *       - Posts
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - authorId
+ *               - title
+ *             properties:
+ *               authorId:
+ *                 type: string
+ *                 description: ID of the author (user)
+ *               title:
+ *                 type: string
+ *                 description: Title of the post
+ *               message:
+ *                 type: string
+ *                 description: Content of the post (optional field)
+ *     responses:
+ *       200:
+ *         description: Post successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   description: The created post object
+ *       400:
+ *         description: Bad request - Missing authorId or title
+ *       500:
+ *         description: Internal server error
+ */
 
-module.exports=router;
+router.post('/writePost', require('../controller/POSTS/savePost'));
+router.patch('/updatePost', require('../controller/POSTS/updatePost'));
+router.get('/getPost', require('../controller/POSTS/fetchPost'));
+router.delete('/deletePost', require('../controller/POSTS/deletePost'));
+
+module.exports = router;
