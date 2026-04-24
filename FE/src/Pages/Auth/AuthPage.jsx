6c16c8793b2001a46c23a2c2b435110d23927445
Kartik Sharma <poojan4604@gmail.com>
2026-04-24 11:32:38 +0530

    fixes and authpage

---
+++ FE/src/Pages/Auth/AuthPage.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+function AuthPage(){
+    const [email,setEmail]=useState('');
+    const [password,setPassword]=useState('');
+    const [name,setName]=useState('');
+    const [loading,setLoading]=useState(false);
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default AuthPage;
