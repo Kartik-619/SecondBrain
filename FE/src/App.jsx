4f6917459398c599e7b27afb7515be1782c03bb1
Kartik Sharma <poojan4604@gmail.com>
2026-04-24 11:19:43 +0530

    login setup

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import InputCard from './Component/inputCard'
 import NavBar from './Component/Navbar'
 import Home from './Pages/Home/Home'
@@ -6,7 +7,13 @@ function App() {
   return (
     <div className="min-h-screen w-screen bg-neutral-700">
       <NavBar />
-      <Home/>
+      <BrowserRouter>
+      <Routes>
+      <Route to={'/'} element={<Home/>}/>
+      <Route to={'/login'} element={<Login/>}/>
+      <Route to={'/register'} element={<Register/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- FE/src/Pages/Home/Home.jsx
+++ FE/src/Pages/Home/Home.jsx
@@ -1,13 +1,25 @@
+import { useNavigate } from "react-router-dom"
 export default function Home(){
+    const navigate=useNavigate();
+    const LoginOpen=()=>{
+        navigate("/login");
+    }
+    const RegisterOpen=()=>{
+        navigate("/register");
+    }
     return(
         <div className="w-screen flex bg-white h-screen justify-center item-center">
             <h1 className="text-black ">Welcome to Second Brain</h1>
-            <div>
+            
                 <div>
-                    <h3 className="flex text-black">
+                    <h3 className="flex item-center text-black">
                         Second Brain is here to keep a hold of your data, wheather keep it as your personal diary or save your Idea.
                     </h3>
                 </div>
+            <div className="flex item-center bg-gray-500 space-between">
+                <button className="p-3 text-black"  onClick={LoginOpen}>Login</button>
+                <button className="p-3 text-black" onClick={RegisterOpen}>Register</button>
+
             </div>
 
         </div>
