b187d2d60a13e8ef087f6e1b6ba16d5fb104d396
Kartik Sharma <poojan4604@gmail.com>
2026-04-28 19:20:52 +0530

    yoo

--- FE/src/Pages/Auth/AuthPage.jsx
+++ FE/src/Pages/Auth/AuthPage.jsx
@@ -1,12 +1,33 @@
 import { useState } from "react";
-function AuthPage(){
+
+function AuthPage({isSignin}){
+    const router=useRouter();
     const [email,setEmail]=useState('');
     const [password,setPassword]=useState('');
     const [name,setName]=useState('');
     const [loading,setLoading]=useState(false);
+    
+    async function handleAuth(){
+        
+        if(!email||!password (!isSignin && !name)){
+            return;
+        }
+
+        try{
+            const endpoint=isSignin ?"signin":"so"
+        }catch(e){
+
+        }
+    }
     return(
         <div>
-
+            <main>
+                <form>
+                    <input placeholder="username"/>
+                    <input placeholder="password"/>
+                    <button type="submit">Submit</button>
+                </form>
+            </main>
         </div>
     )
 }
--- FE/src/Pages/Auth/Login.jsx
+++ FE/src/Pages/Auth/Login.jsx
@@ -1,3 +1,25 @@
 export default function Login(){
-    return <AuthPage isSignin={false}/>
+    const{register, handleSubmit}=useForm<FormValues>({
+        defaultValues:{
+            email:'',
+            password:''
+        }
+    })
+    return(
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input 
+                placeholder="Email"
+                {...register("email",{required:true})}
+                />
+                <input
+                placeholder="password"
+                {...register("password",
+                    {minLength:8,message:'Min 8 characters are required'})}
+                />
+
+                <button type="submit"> Login</button>
+            </form>
+        </div>
+    )
 }
