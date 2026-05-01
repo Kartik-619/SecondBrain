5511299966f7721083d84913c2073b49b4024ea3
Kartik Sharma <poojan4604@gmail.com>
2026-05-01 07:34:00 +0530

    Update login.js

--- BE/controller/Auth/login.js
+++ BE/controller/Auth/login.js
@@ -3,8 +3,44 @@ const { prisma } = require("../../prisma/lib/prisma");
 const LoginController=async (req,res)=>{
     try{
         const {email,password}=req.body;
-        
-    }catch(e){
+        if(!email || !password){
+            return res.status.json({success:false,message:'Fields are incomplete'});
+        }
 
+        const user=await prisma.user.findUnique({
+          where:{
+            email
+          }
+        });
+
+        if(!user){
+            return res.status(400).json({
+                success:false,
+                message:'The user does not exist'
+            });
+        }
+
+        if(password=!user.password){
+            return res.status(401).json({
+                success:false,
+                message:'Password does not match'
+            });
+        }
+
+        return res.status(200).json({
+            success:true,
+            message:'Login successful',
+            username:user.username,
+            id:user.id,
+            email:user.email
+        });
+
+    }catch(e){
+        console.error(e);
+        return res.status(500).json({
+            success:false,
+            message:'Internal Server Error'});
     }
 }
+
+module.exports=LoginController
