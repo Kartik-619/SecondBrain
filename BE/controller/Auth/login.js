b440020e723410f6e251adea049cadb7b2b7915e
Kartik Sharma <poojan4604@gmail.com>
2026-05-03 15:47:58 +0530

    auth

--- BE/controller/Auth/login.js
+++ BE/controller/Auth/login.js
@@ -1,46 +1,65 @@
 const { prisma } = require("../../prisma/lib/prisma");
+const jwt = require('jsonwebtoken');
 
-const LoginController=async (req,res)=>{
-    try{
-        const {email,password}=req.body;
-        if(!email || !password){
-            return res.status(400).json({success:false,message:'Fields are incomplete'});
+const LoginController = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ success: false, message: 'Fields are incomplete' });
         }
 
-        const user=await prisma.user.findUnique({
-          where:{
-            email
-          }
+        const user = await prisma.user.findUnique({
+            where: {
+                email
+            }
         });
 
-        if(!user){
+        if (!user) {
             return res.status(400).json({
-                success:false,
-                message:'The user does not exist'
+                success: false,
+                message: 'The user does not exist'
             });
         }
 
-        if(password!==user.password){
+        if (password !== user.password) {
             return res.status(401).json({
-                success:false,
-                message:'Password does not match'
+                success: false,
+                message: 'Password does not match'
             });
         }
+        const token = jwt.sign({
+            id: user.id,
+            username: user.username,
+            email: user.email
+        }, process.env.JWT_SECRET, {
+            expiresIn: '1h'
+        })
+
+        res.cookie("token",token,{
+           httpOnly:true,
+           secure:false,
+           sameSite:"lax", //prevents CSRF
+           maxAge:7 * 24 * 60 * 60 * 1000,
+        })
 
         return res.status(200).json({
-            success:true,
-            message:'Login successful',
-            username:user.username,
-            id:user.id,
-            email:user.email
+            success: true,
+            token,
+            message: 'Login successful',
+            user: {
+                id: user.id,
+                username: user.username,
+                email: user.username
+            }
         });
 
-    }catch(e){
+    } catch (e) {
         console.error(e);
         return res.status(500).json({
-            success:false,
-            message:'Internal Server Error'});
+            success: false,
+            message: 'Internal Server Error'
+        });
     }
 }
 
-module.exports=LoginController
+module.exports = LoginController
---
+++ BE/middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt=require('jsonwebtoken');
+
+const verifyTOken=(req,res,next)=>{
+    try {
+        const token=req.cookies.token; //extract token from cookie
+        if(!token){
+            return res.status(401).json({
+                success:false,
+                message:"NO token available"
+            }
+            );
+        }
+        const decoded=jwt.verify(token,process.env.JWT_SECRET);
+        req.user=decoded;
+
+        next();
+
+    } catch (error) {
+        return res.status(403).json({
+            success:false,
+            message:"Invalid Authorization, Please try again"
+        });
+    }
+}
+module.exports=verifyTOken;
