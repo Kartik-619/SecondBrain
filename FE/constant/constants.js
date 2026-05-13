d29ee62be81796aeb83afe8ff95468a5aa2d2100
Kartik Sharma <poojan4604@gmail.com>
2026-05-13 11:44:35 +0530

    Update constants.js

--- FE/constant/constants.js
+++ FE/constant/constants.js
@@ -1,6 +1,5 @@
 export const navLinks = [
     { label: "Home", path: "/", public: true },
-    { label: "About", path: "/about", public: true },
 
     { label: "My Posts", path: "/myposts", private: true },
 ];
